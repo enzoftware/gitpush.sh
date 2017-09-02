9a8f1b9ee47f43a03f0593a19b063a1b4877ac00
Enzo Lizama Paredes <lizama.enzo@gmail.com>
2017-09-01 19:25:50 -0500

    alredy done

--- app/src/main/java/com/hackspace/enzoftware/light_sensor/MainActivity.kt
+++ app/src/main/java/com/hackspace/enzoftware/light_sensor/MainActivity.kt
@@ -1,28 +1,58 @@
 package com.hackspace.enzoftware.light_sensor
 
+import android.content.ComponentCallbacks2
+import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import com.hackspace.enzoftware.light_sensor.R
 
 
-class MainActivity  : AppCompatActivity() , SensorEventListener() {
+class MainActivity  : AppCompatActivity() , SensorEventListener {
+
+    var sensor:Sensor ?= null
+    var sensorManager:SensorManager ?= null
+
     override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
 
     }
 
-    override fun onSensorChanged(p0: SensorEvent?) {
-
+    var isRunning = false
+    override fun onSensorChanged(event: SensorEvent?) {
+        if(event!!.values[0] > 40 && !isRunning ){
+            isRunning = true
+            try {
+                var mp = MediaPlayer()
+                mp.setDataSource("http://server6.mp3quran.net/thubti/001.mp3")
+                mp.prepare()
+                mp.start()
+                Toast.makeText(this,"La luz es mayor a 40 lux ",Toast.LENGTH_LONG).show()
+            }catch (ex:Exception){}
+        }
     }
 
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensor = sensorManager!!.getDefaultSensor(Sensor.TYPE_LIGHT)
     }
 
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager!!.registerListener(this,sensor,SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager!!.unregisterListener(this)
+    }
 
 }
