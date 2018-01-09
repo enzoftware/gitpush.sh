15ff46a638508d2a422896b4fd996729419bc022
enzoftware <lizama.enzo@gmail.com>
2018-01-09 13:02:59 -0500

    Initial commit

--- app/src/androidTest/java/com/experiments/enzoftware/moviestickets/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/experiments/enzoftware/moviestickets/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.projects.enzoftware.ankotodo
+package com.experiments.enzoftware.moviestickets
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.projects.enzoftware.ankotodo", appContext.packageName)
+        assertEquals("com.experiments.enzoftware.moviestickets", appContext.packageName)
     }
 }
--- app/src/test/java/com/experiments/enzoftware/moviestickets/ExampleUnitTest.kt
+++ app/src/test/java/com/experiments/enzoftware/moviestickets/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.projects.enzoftware.ankotodo
+package com.experiments.enzoftware.moviestickets
 
 import org.junit.Test
 
